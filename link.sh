9640edaf4c0130a8da60d2edab100a017a1fa35a
小野耕平 <kouhei_ono@ML000074KO.local>
2019-09-02 17:52:32 +0900

    add link.sh

---
+++ link.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.viminfo ~/.viminfo
+ln -sf ~/dotfiles/.vim ~/.vim
+ln -sf ~/dotfiles/.dein.toml ~/.dein.toml
+ln -sf ~/dotfiles/.dein_lazy.toml ~/.dein_lazy.toml
+
