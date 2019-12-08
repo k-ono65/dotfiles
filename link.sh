f3f5414f3e0cdaf48116daf6bc82de5a38e916cb
kohei ono <kohei_ono@micin.jp>
2019-12-08 18:13:41 +0900

    customize vimrc

--- link.sh
+++ link.sh
@@ -8,6 +8,7 @@ if [ ! -d ${HOME}/.zsh ];then mkdir ${HOME}/.zsh; fi
 ln -sf ${basepath}/.zshenv ${HOME}/.zshenv
 ln -sf ${basepath}/.zsh/.zshrc ${HOME}/.zsh/.zshrc
 ln -sf ${basepath}/.zsh/.zshenv ${HOME}/.zsh/.zshenv
+ln -sf ${basepath}/.zsh/.zprofile ${HOME}/.zsh/.zprofile
 ln -snf ${basepath}/.zsh/rc ${HOME}/.zsh/rc
 
 # vim files
